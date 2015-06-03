0ec875485b0735be487b0f2f2f80cfd5c7289485
minfawang <minfa.wang@gmail.com>
2015-06-03 12:41:59 -0700

    remove sh

--- scpHaru.sh
+++
@@ -1,6 +0,0 @@
-scp *.c* mwang5@haru.caltech.edu:/home/mwang5/project/
-scp Makefile mwang5@haru.caltech.edu:/home/mwang5/project/
-scp *.sh mwang5@haru.caltech.edu:/home/mwang5/project/
-
-ssh mwang5@haru.caltech.edu "cd project; sh -x run_project.sh"
-# ssh mwang5@haru.caltech.edu
